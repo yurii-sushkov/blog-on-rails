c67a41d35b4083bc95fe3b9d90ecbcf21ab2de6e
Yurii Sushkov <s.supremus@gmail.com>
2020-04-13 11:15:49 +0300

    redirect_to root_path in case of unauthorized action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
-    include Pundit
-    protect_from_forgery with: :exception
+  protect_from_forgery # with: :exception
+  include Pundit
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    redirect_to root_path
+  end
 end
